eda69b38f1ae176bde12f8bb0cd8742276874080
Brock Tillotson <brocktillotson@6degreeshealth.com>
2019-04-15 16:23:11 -0700

    added cors

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/arithmetics/got_boardgame/controllers"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -32,7 +33,11 @@ func main() {
 
 	fmt.Println(port)
 
-	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	err := http.ListenAndServe(":"+port, handlers.CORS(originsOk, headersOk, methodsOk)(router)) //Launch the app, visit localhost:8000/api
 	if err != nil {
 		fmt.Print(err)
 	}
